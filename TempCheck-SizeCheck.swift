54309565d838ecc0473915b5d0b42c27fc2c2399
PencersGitHub <113054133+PencersGitHub@users.noreply.github.com>
2024-03-19 22:08:58 -0400

    Initial commit

---
+++ TempCheck-SizeCheck.swift
@@ -0,0 +1,9 @@
+
+//  TempCheck-SizeCheck.swift
+//
+//  Created by Spencer Scarlett
+//  Created on 2024-03-20
+//  Version 1.0
+//  Copyright (c) Spencer Scarlett. All rights reserved.
+//
+//  Change me.
